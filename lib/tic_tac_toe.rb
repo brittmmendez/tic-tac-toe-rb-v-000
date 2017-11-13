aec1cb9defa00488f338a3558ce6f6a6ed5a9d5e
Brittany Morris <brittmmendez@gmail.com>
2017-11-13 17:51:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,10 +22,6 @@ def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
@@ -36,6 +32,10 @@ def turn(board)
   else
     turn(board)
   end
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 
