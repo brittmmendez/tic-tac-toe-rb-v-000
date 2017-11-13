be9d259e6a28cdc943e24f685f1089f5bac547c8
Brittany Morris <brittmmendez@gmail.com>
2017-11-13 19:00:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,12 +49,7 @@ def turn_count(board)
   end
 
   def current_player(board)
-    if turn_count(board)%2==0
-      return "X"
-    elsif turn_count(board)%2==1
-      return "O"
-    end
-  end
+     turn_count(board) % 2 == 0 ? "X" : "O"
 
 def won?(board)
   WIN_COMBINATIONS.find do|w_combination|
