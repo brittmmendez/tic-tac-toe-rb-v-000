307980cb5175aea54cffacb1a9ab9562fa44b02b
Brittany Morris <brittmmendez@gmail.com>
2017-11-13 20:21:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -84,4 +84,9 @@ def play(board)
   until over?(board)==true
     turn(board)
   end
+  if won?(board)==true
+  puts "Congrats #{winner(board)}"
+elsif draw?(board)==true
+  puts "Cat's Game!"
+end
 end
